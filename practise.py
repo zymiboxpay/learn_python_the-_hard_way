33f8a5a08f203d748b61603e03a9610a1a2b3595
jude <yiyizym@gmail.com>
2015-01-17 14:01:09 +0800

    add

--- practise.py
+++ practise.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
 
-print 23 / 4
+from sys import argv
+
+script, filename = argv
+
+# print "how old are you?",
+# age = raw_input()
+# print "what\'s your name?",
+# name = raw_input()
+
+# print "your name is %s, and you are %s years old." % (name, age)
+
+print 'the script is: ', script
+print 'the file argv is: ',filename
+
+txt = open(filename,'w+')
+
+print 'the content is: ', txt.read()
+
+txt.write('aaa')
+
+print 'now the file is: ',txt.read()
+
+txt.close()
